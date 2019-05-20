6808c0e16d54acfcd6496d2f6adf9871a737834f
BoWintherHansen <bwha15@student.aau.dk>
2019-05-20 16:30:00 +0200

    Tilføjet rettelser

--- BOLD segmentation code/Implementation_Example.m
+++ BOLD segmentation code/Implementation_Example.m
@@ -13,45 +13,47 @@ tic;
 showMethod = true;
 
 %% ASM segmentation
-for i=1:31
-I=flip(Subject(i).Session(1).T2.left(:,:,2),2);
-
-% ISegmented = ASM_Segmentation_BOLD(I);
+% for i=1:31
+% I=flip(Subject(i).Session(1).T2.left(:,:,2),2);
 % 
-% % Plot
-% Segout = mat2gray(I);
-% for k=1:5
-% Outline = bwperim(ISegmented(k).Seg);
-% Segout(Outline) = 1;
+% % ISegmented = ASM_Segmentation_BOLD(I);
+% % 
+% % % Plot
+% % Segout = mat2gray(I);
+% % for k=1:5
+% % Outline = bwperim(ISegmented(k).Seg);
+% % Segout(Outline) = 1;
+% % end
+% % figure; imshow(Segout,[]);
+% 
+% %% Fiks segmenteringer
+% 
+% segGroundTruth = manSegGroundTruth(i).Subject;
+% segACM = SegmentationACM(i).Subject;
+% segASM = Segmentation(i).Subject;
+% 
+% %% Image registration
+% 
+% echoPlanarImages = flip(Subject(i).Session(1).BOLD.left(:,:,:),2);
+% 
+% [BOLDsequenceGT(i).Subject]=Image_registration_and_BOLD(I,echoPlanarImages,segGroundTruth,showMethod);
+% [BOLDsequenceACM(i).Subject]=Image_registration_and_BOLD(I,echoPlanarImages,segACM,showMethod);
+% [BOLDsequenceASM(i).Subject]=Image_registration_and_BOLD(I,echoPlanarImages,segASM,showMethod);
+% 
+% %Filtrering m. lavpas, cutoff= 0.05
+% [BOLDsequenceGT(i).Subject]= BOLD_filter(BOLDsequenceGT(i).Subject, 0); %[BOLDsequence]=BOLD_filter(BOLDsequence,showPlots)
+% [BOLDsequenceACM(i).Subject]= BOLD_filter(BOLDsequenceACM(i).Subject, 0);
+% [BOLDsequenceASM(i).Subject]= BOLD_filter(BOLDsequenceASM(i).Subject, 0);
+% 
+% 
+% % figure; subplot(2,3,1); plot(BOLDsequence(1).Seg); title('Anterior: BOLD');
+% % subplot(2,3,2); plot(BOLDsequence(2).Seg); title('Lateral: BOLD');
+% % subplot(2,3,3); plot(BOLDsequence(3).Seg); title('Deep: BOLD');
+% % subplot(2,3,4); plot(BOLDsequence(4).Seg); title('Soleus: BOLD');
+% % subplot(2,3,5); plot(BOLDsequence(5).Seg); title('Lateral head of Gatrocnemius: BOLD');
 % end
-% figure; imshow(Segout,[]);
 
-%% Fiks segmenteringer
-
-segGroundTruth = manSegGroundTruth(i).Subject;
-segACM = SegmentationACM(i).Subject;
-segASM = Segmentation(i).Subject;
-
-%% Image registration
-
-echoPlanarImages = flip(Subject(i).Session(1).BOLD.left(:,:,:),2);
-
-[BOLDsequenceGT(i).Subject]=Image_registration_and_BOLD(I,echoPlanarImages,segGroundTruth,showMethod);
-[BOLDsequenceACM(i).Subject]=Image_registration_and_BOLD(I,echoPlanarImages,segACM,showMethod);
-[BOLDsequenceASM(i).Subject]=Image_registration_and_BOLD(I,echoPlanarImages,segASM,showMethod);
-
-%Filtrering m. lavpas, cutoff= 0.05
-[BOLDsequenceGT(i).Subject]= BOLD_filter(BOLDsequenceGT(i).Subject, 0); %[BOLDsequence]=BOLD_filter(BOLDsequence,showPlots)
-[BOLDsequenceACM(i).Subject]= BOLD_filter(BOLDsequenceACM(i).Subject, 0);
-[BOLDsequenceASM(i).Subject]= BOLD_filter(BOLDsequenceASM(i).Subject, 0);
-
-
-% figure; subplot(2,3,1); plot(BOLDsequence(1).Seg); title('Anterior: BOLD');
-% subplot(2,3,2); plot(BOLDsequence(2).Seg); title('Lateral: BOLD');
-% subplot(2,3,3); plot(BOLDsequence(3).Seg); title('Deep: BOLD');
-% subplot(2,3,4); plot(BOLDsequence(4).Seg); title('Soleus: BOLD');
-% subplot(2,3,5); plot(BOLDsequence(5).Seg); title('Lateral head of Gatrocnemius: BOLD');
-end
+load('BOLDsequence_GT_ACM_ASM');
 
 %% Calculate dice
 for k=1:5
@@ -204,6 +206,10 @@ subplot(3,2,1)
 plot(t,meanAnteriorGT, 'LineWidth',1.5); hold on
 plot(t,meanAnteriorACM, 'LineWidth',1.5); hold on
 plot(t,meanAnteriorASM,'LineWidth',1.5);
+a=line([0.5 0.5], [0 2],'Color','k', 'LineStyle', '--');
+b=line([5.5 5.5], [0 2],'Color','k', 'LineStyle', '--');
+a.Color(4) = 0.25;
+b.Color(4) = 0.25;
 xlabel('Time [min]')
 ylabel('Normalised SI [%]')
 legend('Mean BOLD Ground Truth','Mean BOLD ACM','Mean BOLD ASM','location','northwest');
@@ -218,6 +224,10 @@ subplot(3,2,2)
 plot(t,meanLateralGT, 'LineWidth',1.5); hold on
 plot(t,meanLateralACM, 'LineWidth',1.5); hold on
 plot(t,meanLateralASM, 'LineWidth',1.5);
+c=line([0.5 0.5], [0 2],'Color','k', 'LineStyle', '--');
+d=line([5.5 5.5], [0 2],'Color','k', 'LineStyle', '--');
+c.Color(4) = 0.25;
+d.Color(4) = 0.25;
 xlabel('Time [min]')
 ylabel('Normalised SI [%]')
 %legend('Mean BOLD Ground Truth','Mean BOLD ACM','Mean BOLD ASM','location','northwest');
@@ -232,6 +242,10 @@ subplot(3,2,3)
 plot(t,meanDeepGT, 'LineWidth',1.5); hold on
 plot(t,meanDeepACM, 'LineWidth',1.5); hold on
 plot(t,meanDeepASM, 'LineWidth',1.5);
+e=line([0.5 0.5], [0 2],'Color','k', 'LineStyle', '--');
+f=line([5.5 5.5], [0 2],'Color','k', 'LineStyle', '--');
+e.Color(4) = 0.25;
+f.Color(4) = 0.25;
 xlabel('Time [min]')
 ylabel('Normalised SI [%]')
 %legend('Mean BOLD Ground Truth','Mean BOLD ACM','Mean BOLD ASM','location','northwest');
@@ -246,6 +260,10 @@ subplot(3,2,4)
 plot(t,meanSoleusGT, 'LineWidth',1.5); hold on
 plot(t,meanSoleusACM, 'LineWidth',1.5); hold on
 plot(t,meanSoleusASM, 'LineWidth',1.5);
+g=line([0.5 0.5], [0 2],'Color','k', 'LineStyle', '--');
+h=line([5.5 5.5], [0 2],'Color','k', 'LineStyle', '--');
+g.Color(4) = 0.25;
+h.Color(4) = 0.25;
 xlabel('Sec')
 xlabel('Time [min]')
 ylabel('Normalised SI [%]')
@@ -261,6 +279,10 @@ subplot(3,2,5)
 plot(t,meanGastrocGT, 'LineWidth',1.5); hold on
 plot(t,meanGastrocACM, 'LineWidth',1.5); hold on
 plot(t,meanGastrocASM, 'LineWidth',1.5);
+i=line([0.5 0.5], [0 2],'Color','k', 'LineStyle', '--');
+j=line([5.5 5.5], [0 2],'Color','k', 'LineStyle', '--');
+i.Color(4) = 0.25;
+j.Color(4) = 0.25;
 xlabel('Time [min]')
 ylabel('Normalised SI [%]')
 %legend('Mean BOLD Ground Truth','Mean BOLD ACM','Mean BOLD ASM','location','northwest');
@@ -269,4 +291,5 @@ xticks([0 2 4 6 7.5]);
 ylim([0.9 1.1]);
 set(gca,'fontsize', 13);
 title('Gastrocnemius','FontSize', 16);
+
 time = toc;
